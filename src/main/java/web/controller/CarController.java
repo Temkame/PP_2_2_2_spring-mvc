08896ddf3eb005250539772ac8d2831e2a55c8f3
temkame <temkame@yandex.ru>
2022-04-11 23:52:43 +0300

    Commit

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,24 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    CarService carService=new CarService();
+
+    @GetMapping(value = "/cars")
+    public String printCar(@RequestParam(value = "count", required = false, defaultValue = "5")
+                                   int count, ModelMap model) {
+        List<Car> cars = carService.getCarsList(count);
+        model.addAttribute("cars", cars);
+        return "сars";
+    }
+}
---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,21 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarService implements CarServiceImpl {
+
+    @Override
+    public List<Car> getCarsList(int count) {
+        List<Car> carsList = new ArrayList<>();
+        carsList.add(new Car("Astra", "2170", "red"));
+        carsList.add(new Car("Corsa", "2180", "yellow"));
+        carsList.add(new Car("GT", "1117", "white"));
+        carsList.add(new Car("Omega", "2190", "black"));
+        carsList.add(new Car("Vectra", "3102", "silver"));
+        return carsList.stream().limit(count).collect(Collectors.toList());
+    }
+}
